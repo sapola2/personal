5c3eda2dc8e1181f695eb9c677d658e585aa4c49
Oslo <105890831+sapola2@users.noreply.github.com>
2023-02-09 23:56:33 -0300

    Create random esp.lua

---
+++ random esp.lua
@@ -0,0 +1,32 @@
+local folder = game.CoreGui:FindFirstChild("OutlineFolder") or Instance.new("Folder")
+if Enabled == false then
+    folder:Destroy()
+end
+
+if folder.Name == "Folder" then
+	folder.Name = "OutlineFolder"
+	folder.Parent = game.CoreGui
+end
+
+local glass = game:GetService("Workspace").Level.Glass
+
+if getgenv().Enabled == true then 
+    getgenv().Enabled = false
+    getgenv().Enabled = true
+end
+while getgenv().Enabled do
+    task.wait()
+for i,v in pairs(glass:GetChildren()) do
+	if v:IsA("Model") and v.Name == "ExteriorCam" then
+	local outline = Instance.new("Highlight")
+	outline.Name = v.Name
+	outline.FillTransparency = Filltransparency
+	outline.FillColor = Fillcolor
+	outline.OutlineColor = Outlinecolor
+	outline.OutlineTransparency = Outlinetransparency
+	outline.Adornee = v.parent
+	outline.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+	
+end
+end
+end 
