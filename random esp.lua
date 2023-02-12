d7ee9d6f70b0f6ae38e426dad16397209ac29815
Oslo <105890831+sapola2@users.noreply.github.com>
2023-02-11 22:12:46 -0300

    Update random esp.lua

--- random esp.lua
+++ random esp.lua
@@ -1,33 +1,35 @@
+getgenv().enabled = true
+getgenv().fillcolor = Color3.new(0, 0, 0) 
+getgenv().outlinecolor = Color3.new(1, 1, 1) 
+getgenv().filltrans = 0.6
+getgenv().outlinetrans = 0 
+
 local folder = game.CoreGui:FindFirstChild("OutlineFolder") or Instance.new("Folder")
-if Enabled == false then
+if enabled == false then
     folder:Destroy()
 end
 
 if folder.Name == "Folder" then
-	folder.Name = "OutlineFolder"
-	folder.Parent = game.CoreGui
+    folder.Name = "OutlineFolder"
+    folder.Parent = game.CoreGui
 end
 
-local glass = game:GetService("Workspace").Level.Glass
-
-if getgenv().Enabled == true then 
-    getgenv().Enabled = false
-    getgenv().Enabled = true
+if getgenv().enabled == true then 
+    getgenv().enabled = false
+    getgenv().enabled = true
 end
-while getgenv().Enabled do
+while getgenv().enabled do
     task.wait()
-for i,v in pairs(glass:GetChildren()) do
-	local cam = v.ExteriorCam
-	local outline = Instance.new("Highlight")
-        outline.Parent = folder
-	outline.Name = v.Name
-	outline.FillTransparency = Filltransparency
-	outline.FillColor = Fillcolor
-	outline.OutlineColor = Outlinecolor
-	outline.OutlineTransparency = Outlinetransparency
-	outline.Adornee = v.parent
-	outline.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-	
-end
-end
-end 
+    for i, v in pairs(game:GetService("Workspace").Level.Glass:GetChildren()) do
+        local esp = holder:FindFirstChild(v.Name) or Instance.new("Highlight")
+        esp.Name = v.Name
+        esp.Parent = folder
+        esp.FillColor = fillcolor 
+        esp.OutlineColor = outlinecolor    
+        esp.FillTransparency = filltrans
+        esp.OutlineTransparency = outlinetrans
+        esp.Adornee = cam
+        esp.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
+        end
+        end
+    
